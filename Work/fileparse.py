3f0c5fedfbca0b05f081e94779abef59e4cc80bc
Juan <juan@tymit.com>
2020-10-27 19:04:19 +0000

    starting 4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,24 +1,21 @@
 # fileparse.py
 import csv
-from typing import List, Union, Iterable
 
-DictTuple = Union[List, tuple]
-
-
-def parse_csv(lines: Iterable, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False) -> List[DictTuple]:
-    """
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+    '''
     Parse a CSV file into a list of records with type conversion.
-    """
-
+    '''
     if select and not has_headers:
         raise RuntimeError('select requires column headers')
 
     rows = csv.reader(lines, delimiter=delimiter)
+
+    # Read the file headers (if any)
     headers = next(rows) if has_headers else []
 
     # If specific columns have been selected, make indices for filtering and set output columns
     if select:
-        indices = [headers.index(colname) for colname in select]
+        indices = [ headers.index(colname) for colname in select ]
         headers = select
 
     records = []
@@ -28,7 +25,7 @@ def parse_csv(lines: Iterable, select=None, types=None, has_headers=True, delimi
 
         # If specific column indices are selected, pick them out
         if select:
-            row = [row[index] for index in indices]
+            row = [ row[index] for index in indices]
 
         # Apply type conversion to the row
         if types:
@@ -48,14 +45,3 @@ def parse_csv(lines: Iterable, select=None, types=None, has_headers=True, delimi
         records.append(record)
 
     return records
-
-import gzip
-with gzip.open('Data/portfolio.csv.gz', 'rt') as file:
-    port = parse_csv(file, types=[str, int, float])
-
-print(port)
-
-lines = ['name,shares,price', 'AA,100,34.23', 'IBM,50,91.1', 'HPE,75,45.1']
-port = parse_csv(lines, types=[str, int, float], has_headers=True)
-
-print(port)
